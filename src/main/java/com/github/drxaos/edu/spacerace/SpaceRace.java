61b9d1c3f58fea5e6b42753d7e98cb5d22f835a7
xaos <xaos@z-inc.net>
2016-09-09 21:17:44 +0500

    Initial commit

---
+++ src/main/java/com/github/drxaos/edu/spacerace/SpaceRace.java
@@ -0,0 +1,34 @@
+package com.github.drxaos.edu.spacerace;
+
+import com.github.drxaos.spriter.Spriter;
+
+public class SpaceRace {
+
+    double player_x, player_y, player_vx, player_vy;
+    double computer1_x, computer1_y, computer1_vx, computer1_vy;
+    double computer2_x, computer2_y, computer2_vx, computer2_vy;
+    double computer3_x, computer3_y, computer3_vx, computer3_vy;
+    double[] asteroid_x, asteroid_y, asteroid_vx, asteroid_vy;
+    double[] wall_x, wall_y;
+
+    public static void main(String[] args) {
+
+        // TODO Отрефакторить в ООП так, чтобы код можно было читать как рассказ об игре
+
+        // 1. Составить словарь предметной области
+        // 2. Выстроить иерархию классов и пакетов
+        // 3. Описать свойства и поведение классов
+        // 4. Найти код не вошедший в поведение классов и добавить вспомогательные классы
+        //      a. Создание и связывание объектов
+        //      b. Поведение внешней среды
+        //      c. Ввод-вывод данных
+        //      d. и т.д.
+
+
+        Spriter spriter = new Spriter("Space Race");
+
+
+
+    }
+
+}
